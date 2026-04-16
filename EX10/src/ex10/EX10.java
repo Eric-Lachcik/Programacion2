aec5a3f549fb6f6b946697438a54accffece9063
Eric Lachcik <lachcikeric@gmail.com>
2026-04-16 16:06:38 +0200

    ex10 empezado

---
+++ EX10/src/ex10/EX10.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex10;
+
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author blu20
+ */
+public class EX10 extends JFrame implements MouseListener {
+
+    Tauler tauler;
+    int numReines = 0;
+
+    public EX10() {
+        super("Joc de les reines");
+        tauler = new Tauler();
+        tauler.addMouseListener(this);
+        this.getContentPane().add(tauler);
+        this.pack();
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+        EX10 esc = new EX10();
+        esc.setVisible(true);
+    }
+    
+    @Override
+    public void mouseClicked(MouseEvent e) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        int i, j = 0;
+        if (e.getButton() == MouseEvent.BUTTON1 && numReines < 8) {
+ 
+//         Obtindre casella clicada (observar que i,j son enters)
+            i = e.getY() / Tauler.COSTAT; // Fila 
+            j = e.getX() / Tauler.COSTAT; // Columna
+            
+//            Codi antic 
+//            Obtindre casella clicada
+//            int x = e.getX();
+//            int y = e.getY();
+//            boolean trobat = false;
+//            for (i = 0; i < Tauler.DIMENSIO && !trobat; i++) {
+//                for (j = 0; j < Tauler.DIMENSIO && !trobat; j++) {
+//                    trobat = tauler.dinsCasella(i, j, x, y);
+//                }
+//            }
+//            i--;
+//            j--;
+            System.out.println("Click a: " + i + ", " + j);
+            if (!tauler.isOcupat(i, j)) {
+                tauler.Posa(Pesa.REINAN, i, j);
+                numReines++;
+            } else {
+                Toolkit.getDefaultToolkit().beep();
+                tauler.buida(i, j);
+                numReines--;
+            }
+            tauler.repaint();
+//            tauler.paintImmediately(tauler.getRectangle(i, j));
+            if (numReines == 8) {
+                Toolkit.getDefaultToolkit().beep();
+                ImageIcon icon = new ImageIcon("peces/cavallN.png");
+                JOptionPane.showMessageDialog(null, "8 Reines al tauler!",
+                        "Victoria!", JOptionPane.INFORMATION_MESSAGE, icon);
+            }
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ EX10/src/ex10/Tauler.java
@@ -0,0 +1,93 @@
+/*
+ * Classe Tauler definida com un panell on es defineix una taula 8x8 de caselles
+ *
+ * El constructor defineix totes les caselles com a rectangles d'un color
+ * determinat inicialitzant-les buides
+ *
+ * El mètode paintComponent recorr el tauler pintant les caselles
+ *
+ * El mètode getPreferredSize retorna el tamany del tauler
+ *
+ * El mètode posa col·loca una peça a la posició que indiquen els dos enters,
+ * les peces són les constants definides per la classe Pesa
+ */
+package ex10;
+
+/**
+ *
+ * @author miquelmascaro
+ * Tomeu Estrany: Canviat visió de les constants MAXIM, COSTAT, BLANC, NEGRE
+ * ara són públiques
+ */
+import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+
+public class Tauler extends JPanel {
+
+    public static final int DIMENSIO = 8;
+    
+    public static final int MAXIM = 800;
+    public static final int COSTAT = MAXIM / DIMENSIO;
+    public static final Color BLANC = Color.LIGHT_GRAY;
+    public static final Color NEGRE = Color.DARK_GRAY;
+    private Casella t[][];
+
+    public Tauler() {
+        t = new Casella[DIMENSIO][DIMENSIO];
+        int y = 0;
+        for (int i = 0; i < DIMENSIO; i++) {
+            int x = 0;
+            for (int j = 0; j < DIMENSIO; j++) {
+                Rectangle2D.Float r =
+                        new Rectangle2D.Float(x, y, COSTAT, COSTAT);
+                Color col;
+                if ((i % 2 == 1 && j % 2 == 1) || (i % 2 == 0 && j % 2 == 0)) {
+                    col = BLANC;
+                } else {
+                    col = NEGRE;
+                }
+                t[i][j] = new Casella(r, col, false);
+                x += COSTAT;
+            }
+            y += COSTAT;
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        for (int i = 0; i < DIMENSIO; i++) {
+            for (int j = 0; j < DIMENSIO; j++) {                
+                t[i][j].paintComponent(g);
+            }
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(MAXIM, MAXIM);
+    }
+
+    void Posa(String s, int i, int i0) {
+        t[i][i0].setPesa(new Pesa(s));
+    }
+
+    boolean dinsCasella(int i, int j, int x, int y) {
+        return t[i][j].getRec().contains(x, y);
+    }
+
+    boolean isOcupat(int i, int j) {
+        return t[i][j].isOcupada();
+    }
+
+    void buida(int i, int j) {
+        t[i][j].setOcupada(false);
+    }
+
+    Rectangle getRectangle(int i, int j) {
+        return t[i][j].getRec().getBounds();
+    }
+}
